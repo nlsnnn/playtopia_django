8cadcf8cc01afd3d84a00bec3af634ae5e2ab887
nlsnnn <lagged.06@mail.ru>
2024-05-27 16:31:00 +0300

    add base admin panel

--- playtopia/store/admin.py
+++ playtopia/store/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Category, Product
 
-# Register your models here.
+
+admin.site.site_header = 'Админ панель'
+admin.site.index_title = 'Playtopia'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ['time_create', 'time_update']
+    list_display = ['name', 'game_image', 'price', 'category', ]
+
+
+    @admin.display(description='Фото')
+    def game_image(self, product: Product):
+        if product.photo:
+            return mark_safe(f"<img src='{product.photo.url}' width=50>")
+        return 'Без фото'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug']
+    list_display = ['id', 'name']
