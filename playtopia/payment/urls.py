047a84a31c4a54e07211bb787e3acd7d9f042021
nlsnnn <lagged.06@mail.ru>
2024-06-08 20:10:15 +0300

    finish payment

--- playtopia/payment/urls.py
+++ playtopia/payment/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('checkout/', views.Checkout.as_view(), name='checkout')
+    path('checkout/', views.Checkout.as_view(), name='checkout'),
+    path('complete-order/', views.complete_order, name='complete_order'),
+    path('success/', views.payment_success, name='payment_success'),
+    path('fail/', views.payment_fail, name='payment_fail')
 ]
--- playtopia/payment/views.py
+++ playtopia/payment/views.py
@@ -1,11 +1,14 @@
 from typing import Any
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render
 from django.views.generic import UpdateView
 
 from cart.cart import CartApp
+from store.models import Product
 
+from .models import Order, OrderItem
 from .forms import CheckoutForm
 
 
@@ -22,3 +25,30 @@ class Checkout(UpdateView):
         cart = CartApp(self.request)
         context['sum'] = cart.get_total_price()
         return context
+
+
+def complete_order(request: HttpRequest):
+    if request.POST.get('action') == 'payment':
+        user = request.user
+        amount = request.POST.get('amount')
+        cart = CartApp(request)
+
+        order = Order.objects.create(user=user, amount=amount)
+
+        for item in cart.user_cart:
+            product = Product.objects.get(pk=item.product_id)
+            OrderItem.objects.create(order=order, product=product,
+                                     price=product.price, quantity=item.quantity)
+
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False})
+
+
+def payment_success(request: HttpRequest):
+    cart = CartApp(request)
+    cart.user_cart.all().delete()
+
+    return render(request, 'payment/payment-success.html')
+
+def payment_fail(request: HttpRequest):
+    return render(request, 'payment/payment-fail.html')
