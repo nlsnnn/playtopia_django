93a760a16047cf95411813f66e3c995401a8ec8f
nlsnnn <lagged.06@mail.ru>
2024-07-03 14:27:47 +0300

    refactor payment models

--- playtopia/payment/models.py
+++ playtopia/payment/models.py
@@ -5,10 +5,18 @@ from store.models import Product
 
 class Order(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
+    email = models.EmailField(verbose_name='Email')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Заказ {self.id}'
+
+    class Meta:
+        ordering = ['-created']
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
